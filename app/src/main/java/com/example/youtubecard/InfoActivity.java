ede71b5a4112549961679a49f599f2eccca0a5ba
shubham100ev <shubhamsingh19981110@gmail.com>
2019-03-11 17:16:48 +0530

    Android day 5

--- app/src/main/java/com/example/youtubecard/InfoActivity.java
+++ app/src/main/java/com/example/youtubecard/InfoActivity.java
@@ -2,12 +2,27 @@ package com.example.youtubecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
+
+
+    Button commentbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        commentbtn=findViewById(R.id.com);
+        commentbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Comment Done",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/youtubecard/MainActivity.java
+++ app/src/main/java/com/example/youtubecard/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.youtubecard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img1 = findViewById(R.id.img1);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openinfoact();
+            }
+        });
+    }
+
+    public void openinfoact() {
+        Intent intent = new Intent(this, InfoActivity.class);
+        startActivity(intent);
     }
 }
